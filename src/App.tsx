20096c6a2088eb685d9a77e8a34556081941becf
KING-REX <anijimjamesmaurice@gmail.com>
2024-01-05 09:00:14 +0100

    Changed to typescript

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "./logo.svg";
+// import "./App.css";
+import "./css/App.css";
+
+function App() {
+	return <div className="App"></div>;
+}
+
+export default App;
